792f9564677095b6d561bd16a22cfb22f8370dfa
Ranjot Singh <61102985+Ranjot24@users.noreply.github.com>
2025-01-03 14:16:29 +0530

    Update [id].js

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -2,11 +2,10 @@ import axios from "axios";
 import styles from "../../styles/MovieDetails.module.css";
 
 export default function MovieDetails({ movie }) {
-
   const handleTrailerClick = async () => {
     try {
       const { data } = await axios.get(
-        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=2cce486e7f54a137ac4e291d967e29aa`
+        `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=2cce486e7f54a137ac4e291d967e29aa`
       );
       const trailer = data.results.find((video) => video.type === "Trailer");
       if (trailer) {
@@ -18,30 +17,21 @@ export default function MovieDetails({ movie }) {
       console.error("Error fetching trailer:", error.message);
     }
   };
-  
+
   return (
     <div className={styles.container}>
       <h1 className={styles.h1}>{movie.title}</h1>
       <img
         src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-        alt={movie.title} className={styles.img}
+        alt={movie.title}
+        className={styles.img}
       />
       <p className={styles.p}>{movie.overview}</p>
       <p className={styles.p}>Release Date: {movie.release_date}</p>
       <p className={styles.p}>Rating: {movie.vote_average}</p>
-        <button
-          className={styles.trailerButton}
-          onClick={async () => {
-            const trailerUrl = await handleTrailerClick(movie.id);
-            if (trailerUrl) {
-              window.open(trailerUrl, "_blank");
-            } else {
-              alert("Trailer not available for this movie.");
-            }
-          }}
-        >
-          Watch Trailer
-        </button>
+      <button className={styles.trailerButton} onClick={handleTrailerClick}>
+        Watch Trailer
+      </button>
     </div>
   );
 }
