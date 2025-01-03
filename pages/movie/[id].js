ea43d4e73dcfc23329b3951db207e2e73fed110e
Ranjot Singh <61102985+Ranjot24@users.noreply.github.com>
2025-01-03 14:00:48 +0530

    Update [id].js

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -2,6 +2,23 @@ import axios from "axios";
 import styles from "../../styles/MovieDetails.module.css";
 
 export default function MovieDetails({ movie }) {
+
+  const handleTrailerClick = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}/videos?api_key=2cce486e7f54a137ac4e291d967e29aa`
+      );
+      const trailer = data.results.find((video) => video.type === "Trailer");
+      if (trailer) {
+        window.open(`https://www.youtube.com/watch?v=${trailer.key}`, "_blank");
+      } else {
+        alert("No trailer available for this movie.");
+      }
+    } catch (error) {
+      console.error("Error fetching trailer:", error.message);
+    }
+  };
+  
   return (
     <div className={styles.container}>
       <h1 className={styles.h1}>{movie.title}</h1>
@@ -12,6 +29,9 @@ export default function MovieDetails({ movie }) {
       <p className={styles.p}>{movie.overview}</p>
       <p className={styles.p}>Release Date: {movie.release_date}</p>
       <p className={styles.p}>Rating: {movie.vote_average}</p>
+        <button className={styles.trailerButton} onClick={handleTrailerClick}>
+        Watch Trailer
+      </button>
     </div>
   );
 }
